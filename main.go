fad37d1d540d0356086205f8e3f1bdc122287598
cadaverine <kennville@gmail.com>
2018-09-12 13:02:23 +0300

    add 'some' function

--- main.go
+++ main.go
@@ -6,7 +6,21 @@ import (
 	"./stack"
 )
 
+func some(slice []interface{}, predicat func() bool) bool {
+	for _, value := range slice {
+		if predicat(value) {
+			return true
+		}
+	}
+	return false
+}
+
 func checkBrackets(text string) bool {
+	for i, char := range text {
+		if some(['{', '(', '['], func (value rune) { return value == char }) {
+
+		}
+	}
 
 	return false
 }
